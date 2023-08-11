f5599279a03a76220733f57b4b55d8c498ea09b3
Duaa-salah <dabulfetouh@gmail.com>
2023-08-11 16:59:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: 0
+*/
+int main(void)
+{
+	char alph = 'a';
+	
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	alph = 'A';
+	while (alph <= 'Z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
