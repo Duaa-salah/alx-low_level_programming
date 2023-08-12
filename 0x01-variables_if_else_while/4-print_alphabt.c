d4e83b80afb6e2e38995c21d6dd995034972a043
Duaa-salah <dabulfetouh@gmail.com>
2023-08-11 17:10:59 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: 0
+*/
+int main(void)
+{
+        char alph = 'a';
+
+        while (alph <= 'z')
+        {
+                putchar(alph);
+                alph++;
+	if (alph == 'e' || alph == 'q')
+		alph++;
+        }
+        putchar('\n');
+        return (0);
+}
