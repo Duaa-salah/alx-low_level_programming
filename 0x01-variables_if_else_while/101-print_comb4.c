f7c7f1cfa94a1299adff366c67c646bcc179b5ef
Duaa-salah <dabulfetouh@gmail.com>
2023-08-13 02:01:19 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,36 +6,35 @@
  */
 int main(void)
 {
-        int d1 = 0;
-        int d2;
+	int d1 = 0;
+	int d2;
 	int d3;
-
-        while (d1 <= 9)
-        {
-                d2 = 0;
-                while (d2 <= 9)
-                {
+	
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
 			d3 = 0;
-			while (d3 <=9)
+			while (d3 <= 9)
 			{
-				
-                        	if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3 && d1 != d3)
-                        	{
-                                	putchar(d1 + 48);
-                                	putchar(d2 + 48);
+				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3 && d1 != d3)
+				{
+					putchar(d1 + 48);
 					putchar(d2 + 48);
-                        	if (d1 + d2 + d3 != 24)
-                        		{
-                                	putchar(',');
-                                	putchar(' ');
-                        		}
-                        	}
+					putchar(d2 + 48);
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 				d3++;
 			}
-                d2++;
-                }
-                d1++;
-        }
-        putchar('\n');
-        return (0);
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
 }
