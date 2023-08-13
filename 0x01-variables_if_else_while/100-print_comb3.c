e729c832bdc848e3ca47ee8675dd5524eb5eb5fc
Duaa-salah <dabulfetouh@gmail.com>
2023-08-13 01:31:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - main entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int d1=0;
+	int d2;
+
+	while (d1 <= 9)
+	{
+		d2=0;
+		while (d2 <= 9)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1+ 48);
+				putchar(d2+ 48);
+				
+			if (d1 != 8 && d2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+	return (0);
+}
