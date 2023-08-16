4f634a8bc145e3b5bb6617d9051652ad1bb3b2d7
Duaa-salah <dabulfetouh@gmail.com>
2023-08-16 15:38:41 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,27 @@
 #include "main.h"
 
 /**
- *_isalpha - check if the given character is lower case
+ * print_sign  - check if the given character is +/-
  *
- * @c: charachter
+ *
  * Return: 0 if uppercase, or 1 if lowercase
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	else 
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 
 }
