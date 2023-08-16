5b20c84ed2087943d3eee4e08b49fd6090c84f3d
Duaa-salah <dabulfetouh@gmail.com>
2023-08-16 14:22:38 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * _islower - tests whether a character is a
- * lowercase letter from the English alphabet
+ * _islower - tests whether a character is lowercase
  * return: 0 if uppercase, or 1 if lowercase
  * @c: charachter
 */
