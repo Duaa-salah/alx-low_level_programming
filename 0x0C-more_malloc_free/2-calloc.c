8b278452edb5841c57ee818ed3258f64ddaddcec
Duaa-salah <dabulfetouh@gmail.com>
2023-09-07 17:53:08 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,13 +5,17 @@
  * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
- *
+ * *char_f - Initialize the allocated memory to zero
+ * *x - Allocate memory using malloc
+ * @tsize: Calculate the total size in bytes
  * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	size_t tsize = nmemb * size;
 	void *x;
+	size_t a;
+	char *char_f;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -24,7 +28,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	memset(x, 0, tsize);
-	
+
+	char_f = (char *)x;
+
+	for (a = 0; a < tsize; a++)
+	{
+		char_f[a] = 0;
+	}
+
 	return (x);
 }
