b00db24b5df1926c4ba3dfa52d55243d615108b4
Duaa <dabulfetouh@gmail.com>
2023-11-17 06:59:11 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
